8ab784670a2a94507db311f1e3284665f42d7ae8
smzainrazauit67 <62296942+smzainrazauit67@users.noreply.github.com>
2023-02-01 07:22:34 +0500

    Updated mysqllog server
    Socket.io is implemented to send data from mysql in streams

--- mysql-server.js
+++ mysql-server.js
@@ -1,13 +1,58 @@
 const express = require('express');
 const app = express();
-// const http = require("http");
-const mysql = require('mysql');
+const http = require("http");
 const cors = require('cors');
 const bodyParser = require('body-parser');
-// const fs = require('fs');
-// const tm = require( 'text-miner');
-
+const fs = require('fs');
+const mysql = require('mysql');
+// const spawn = require('child_process').spawn;
+// const { exec } = require('child_process');
+let records;
+const server = http.createServer(app);
 const port = process.env.PORT || 4120;
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log("Socket Connected");
+
+  socket.on("mysql-logs", (data) => {
+    console.log("=== creating stream ===");
+    // .on('data', (chunk) => {
+    //   const lines = chunk.toString().split('\n').slice(-10);
+    //   // console.log(lines)
+    for (let i = 0; i < 15; i++) {
+      setTimeout(() => {
+        console.log(typeof records)
+        if (records) socket.emit('mysql-logs', records[i]);
+        }, i * 1500);
+      }
+  });
+  
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+    });
+
+  });
+  
+  // fs.watch('/var/log/syslog', (eventType, filename) => {
+  //   if (eventType === 'change') {
+  //     console.log(`File ${filename} was changed!`);
+  //     exec('tail -n 1 /var/log/syslog', (err, stdout, stderr) => {
+  //       if (err) {
+  //         console.error(err);
+  //         return;
+  //       }
+  //       // console.log(stdout);
+  //       socket.emit('linux-logs', { lineData: stdout, lineNum: 0});
+  //     });
+  //   }
+  // });
+
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -15,7 +60,7 @@ app.use(bodyParser.json());
 
 app.post('/api/sqlconnection', function (req, res) {
   const { host, user, password, database } = req.body;
-  console.log('User Details: ', req.body);
+  // console.log('User Details: ', req.body);
   const connection = mysql.createConnection({
     user: user,
     host: host,
@@ -31,13 +76,13 @@ app.post('/api/sqlconnection', function (req, res) {
     console.log('Connection: Established sucessfully'); 
     // res.send('Connection: Established sucessfully');
   })
-  connection.query("SELECT event_time, user_host, server_id, command_type FROM general_log WHERE command_type='Query' LIMIT 15", function (err, result) {
+  connection.query("SELECT event_time, user_host, server_id, command_type FROM general_log LIMIT 15", function (err, result) {
     if (err) {
         console.log('Error on query: ' + err.message);
         return;
         }
       console.log("Query: Successful");
-      const records = result;
+      records = result;
       console.log('# of records: ', records.length);
       res.send(records);
 })
@@ -45,4 +90,8 @@ app.post('/api/sqlconnection', function (req, res) {
 
 app.listen(port, () => {
   console.log(`App started on port: ${port}`)
+})
+
+server.listen(port+10, () => {
+  console.log(`Socket port: ${port+10}`)
 })
