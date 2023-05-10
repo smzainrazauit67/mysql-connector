f21977391ac2046ca11974f1009f2a3a65168047
smzainrazauit67 <62296942+smzainrazauit67@users.noreply.github.com>
2023-05-10 09:30:37 +0500

    Backend updated with post api for data archiving and user history

--- mysql-server.js
+++ mysql-server.js
@@ -10,6 +10,8 @@ const mysql = require('mysql');
 let records;
 const server = http.createServer(app);
 const port = process.env.PORT || 4120;
+const axios = require("axios");
+const moment = require("moment");
 
 const io = require("socket.io")(server, {
   cors: {
@@ -48,12 +50,12 @@ io.on('connection', (socket) => {
   });
 
   socket.on("watch-logs", () => {
-    // fs.watchFile('C:/ProgramData/MySQL/MySQL Server 8.0/Data/mysql/general_log.CSV', (curr, prev) => {
-      fs.watchFile('/var/lib/mysql/mysql/general_log.CSV', (curr, prev) => {
+    fs.watchFile('C:/ProgramData/MySQL/MySQL Server 8.0/Data/mysql/general_log.CSV', (curr, prev) => {
+      // fs.watchFile('/var/lib/mysql/mysql/general_log.CSV', (curr, prev) => {
         if (curr.mtime !== prev.mtime ) {
           console.log(`Change Detected!`); 
-          // fs.createReadStream("C:/ProgramData/MySQL/MySQL Server 8.0/Data/mysql/general_log.CSV")
-          fs.createReadStream("/var/lib/mysql/mysql/general_log.CSV")
+          fs.createReadStream("C:/ProgramData/MySQL/MySQL Server 8.0/Data/mysql/general_log.CSV")
+          // fs.createReadStream("/var/lib/mysql/mysql/general_log.CSV")
           .on('data', (chunk) => {
             const lines = chunk.toString().split('\n').slice(-2);
             const resLines = {resLine: lines}
@@ -92,8 +94,8 @@ io.on('connection', (socket) => {
 app.post('/api/sqlconnection', function (req, res) {
   const { host, user, password, database } = req.body;
   // console.log('User Details: ', req.body);
-  // fs.chmod('C:/ProgramData/MySQL/MySQL Server 8.0/Data/mysql/general_log.CSV', 0o777, (err) => {
-  fs.chmod('/var/lib/mysql/mysql/general_log.CSV', 0o777, (err) => {
+  fs.chmod('C:/ProgramData/MySQL/MySQL Server 8.0/Data/mysql/general_log.CSV', 0o777, (err) => {
+  // fs.chmod('/var/lib/mysql/mysql/general_log.CSV', 0o777, (err) => {
     if (err) throw err;
     console.log('File permissions changed successfully!');
   });
@@ -103,7 +105,7 @@ app.post('/api/sqlconnection', function (req, res) {
     password: password,
     database: database,
     insecureAuth : true,
-    socketPath: '/var/run/mysqld/mysqld.sock'
+    // socketPath: '/var/run/mysqld/mysqld.sock'
   });
   connection.connect((error) => {
     if(error) {
@@ -113,11 +115,26 @@ app.post('/api/sqlconnection', function (req, res) {
     console.log('Connection: Established sucessfully'); 
     // res.send('Connection: Established sucessfully');
   })
-  connection.query("SELECT DATE_FORMAT(event_time, '%d-%b-%Y %H:%i:%s') AS et, server_id, command_type ,CONVERT(argument USING utf8) AS qu FROM general_log order by event_time desc LIMIT 10;", function (err, result) {
+  connection.query("SELECT DATE_FORMAT(event_time, '%d-%b-%Y %H:%i:%s') AS et, server_id, command_type ,CONVERT(argument USING utf8) AS qu FROM general_log order by event_time desc LIMIT 50;", function (err, result) {
     if (err) {
         console.log('Error on query: ' + err.message);
         return;
         }
+        const currentTime = moment().format('MMMM Do YYYY hh:mm:ss a');
+        const strResult = JSON.stringify(result);
+        // console.log(strResult);
+        axios.post("http://172.104.174.187:4000/api/add-history", 
+        {
+          id: 22, 
+          con_type: "MySQL", 
+          timestamp: currentTime
+        });
+        axios.post("http://172.104.174.187:4000/api/set/arch-logs", 
+        { 
+          user_id: 22,
+          data_src: "MySQL",
+          log_data: strResult
+        });
       console.log("Query: Successful");
       records = result;
       console.log('# of records: ', records.length);
